e3dcadbed6e06beae0e7a0e688bd2eaf7a22c2d9
Luu Xuan Dat 197 <lx.xuandat@gmail.com>
2021-03-30 22:57:52 +0700

    Outputting Data Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function getindex()
+    {
+        $products = Product::all();
+        return view('shop.index', [
+            'products' => $products,
+        ]);
+    }
 }
